ce31325292c6f23f6f11360d2dbb90b40aa356c7
wmartinezm <wlsn.martinez@gmail.com>
2023-09-27 16:49:50 -0600

    Updating project

--- test/test_signal.c
+++ test/test_signal.c
@@ -127,20 +127,31 @@ void signal_handle_calculation(struct k_sem *request,
                                struct k_sem *response,
                                struct signal_data *data)
 {
-    k_sem_take(request, K_MSEC(1000));
+    printf("+ Waiting for request\n");
+    k_sem_take(request, K_FOREVER);
+    printf("+ Handling calculation\n");
     data->output = data->input + 5;
     k_sem_give(response);
+    printf("+ Done with calculation\n");
 }
 
 int signal_request_calculate(struct k_sem *request,
                              struct k_sem *response,
                              struct signal_data *data)
 {
+    printf("- Handoff to worker\n");
     k_sem_give(request);
-    if (!k_sem_take(response, K_FOREVER))
-    return 0;
-    else
-    return 1;
+    printf("- Waiting for results\n");
+    if (!k_sem_take(response, K_MSEC(1000)))
+    {
+         printf("- Result ready\n");
+        return 0;
+    }
+    
+    else{
+         printf("- Result failed\n");
+        return 1;
+    }
 }
 
 int main (void)
